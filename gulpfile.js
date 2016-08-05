48e2944137f3faa5bb8307c9216a24d614bcbd4f
François Drouin-Morin <francois.drouinm@gmail.com>
2016-08-05 00:33:57 -0400

    Tooling tweaking

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp');
 var del = require('del');
 var gutil = require('gulp-util');
 var tslint = require('gulp-tslint');
+var webpack = require('gulp-webpack');
 
 gulp.task('default', function(){
-    gutil.log("Hello gulp!");
+    return gutil.log("Hello gulp!");
 });
 
 gulp.task('purge', function(){
@@ -15,11 +16,18 @@ gulp.task('purge', function(){
 });
 
 gulp.task('tslint', function(){
-    gulp.src(['client/**/*.ts?', 'server/**/*.ts?'])
+    return gulp.src(['client/**/*.ts?', 'server/**/*.ts?'])
         .pipe(tslint({formatter: "verbose"}))
         .pipe(tslint.report({emitError: false}));
 });
 
+gulp.task('webpack', function(){
+    return gulp.src('client/index.tsx')
+        .pipe(webpack(require('./webpack.config')))
+        .pipe(gulp.dest('.'))
+});
+
 gulp.task('watch', function(){
-   gulp.watch(['client/**/*.ts?', 'server/**/*.ts?'], ['tslint'])
+    //gulp.watch(['client/**/*.ts?', 'server/**/*.ts?'], ['tslint']);
+    gulp.watch(['client/**/*.ts?'], ['webpack']);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 var webpack = require('webpack');
 var VendorChunkPlugin = require('webpack-vendor-chunk-plugin');
 
-module.exports = [{
+module.exports = {
     entry: {
         app : "./client/index.tsx",
         vendor: ['react']
     },
     output: {
-        filename: "./public/[name].js",
+        filename: "./public/[name].js"
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -28,15 +28,11 @@ module.exports = [{
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { test: /\.js$/, loader: "source-map-loader" }
         ]
-    },
-    plugins:[
-        new webpack.optimize.CommonsChunkPlugin('vendor', './public/vendor.js'),
-        new VendorChunkPlugin('vendor')
-    ],
+    }
 
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
 
-}];
+};
